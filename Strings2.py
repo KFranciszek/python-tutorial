1d1e2053a5567f1656bddc2ea54dee78a8fed25e
Pes2009 <mmadej@sigma.ug.edu.pl>
2016-03-05 20:26:51 +0100

    Python Lekcja 7 string cz2.

---
+++ Strings2.py
@@ -0,0 +1,17 @@
+"hey now"
+print "hey now"
+
+num = 18
+
+print num + 18
+
+ # errorprint "martin id" + num
+
+print "martin id is: " + str(18) # good 
+
+num2 = 32
+
+# error nie mozna int + string laczyc jako string print "my mom is " + num2
+
+print "mym mom is " + `num2` 
+
