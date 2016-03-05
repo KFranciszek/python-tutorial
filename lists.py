64106fb2b3c191b559302d545e0f0d6021a3b646
Pes2009 <mmadej@sigma.ug.edu.pl>
2016-03-05 21:20:36 +0100

    listy/tuple cos ala tablice w cpp

---
+++ lists.py
@@ -0,0 +1,9 @@
+family = ['mom','dad','bro','sis','dog' ] # tupla czyli lista ~ tablica z c++
+
+print family [1] # wyswietli pierwsze z listy 
+
+print family [:3] # wyswietli od pierwszego do 3 ale bez niego z listy
+
+print family [2:] # wyswietli wszystko procz dwoch pierwszych
+
+print "bucky" [3] # wyswietli 4 literke bo indeksujemy od zera dziala tez na str jak widac
