3c0cb19fbb94d648e9bad5cba4de79a6a3c5874f
Pes2009 <mmadej@sigma.ug.edu.pl>
2016-03-05 21:55:58 +0100

    lekcja 11 sekwencje list i nie tylko.

---
+++ Editing-Sequences.py
@@ -0,0 +1,21 @@
+print [7,4,5] + [4,6,5] # sekwencje mozemy dodawac
+
+print "Martin " + "Madej" # slowa tez ( stringi )
+
+print [4,5,6] + ["Martin"] # liczby z slowami tez jesli sa oba jako listy zapisane
+
+print 'Martin'*10 # wyswietli 10x ten napis 
+
+print 21 * 10
+
+print [21] * 10 # wyswietli liste gdzie jest 10 razy "21"
+
+name = "roastbeef"
+print 'z' in name # zwraca prawde/falsz nam czy jest literka 'z' w zmiennej " name "
+
+print 'r' in name # zwraca prawde/falsz nam czy jest literka 'r' w zmiennej " name "
+
+family = ['mom','dad','bro']
+print "sis" in family # sprawdza czy jest zmienna "sis" w liscie i zwraca prawde/falsz
+
+print "mom" in family # sprawdza czy jest zmienna "mom" w liscie i zwraca prawde/falsz
