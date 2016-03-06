ebc12cf225f34a8bdf00dc0f173f7c65d56a3f9a
Pes2009 <mmadej@sigma.ug.edu.pl>
2016-03-07 00:01:13 +0100

    sortowanie list itp./a.out strona124.cpp

---
+++ sort-and-tuples.py
@@ -0,0 +1,17 @@
+new = [32,54,22,7,98,1]
+
+print new
+
+new.sort() # sortuje od najmniejszej do najwiekszej
+print new 
+
+print sorted('Easyhoss') # zamienia stringi na pojedyncze literki i je sortuje
+
+print 41,42,32,54
+
+bucky = (32,32,43,54)
+
+
+print bucky
+
+print bucky[2]
