0fdcaa7fabae78292cc610b87fa4fb00fe465dba
Pes2009 <mmadej@sigma.ug.edu.pl>
2016-03-07 00:23:24 +0100

    nowiuski

---
+++ nowy.py
@@ -0,0 +1,3 @@
+print "hey now brown cow"
+x = raw_input("Enter name:")
+print "Hey " + x
---
+++ strings_n_stuff.py
@@ -0,0 +1,17 @@
+bucky = "hey there %s, hows your %s"
+varb=('betty','foot')
+
+print bucky % varb
+
+varc = ('tuna','fin')
+ 
+print bucky % varc
+
+example = "hey now bessie nice chops"
+
+print example.find('bessie') # od ktorego znaku zaczyna sie bessie slowo
+
+print example.find('chops') # jak wyzej 
+
+seperator = 'hoss'
+
