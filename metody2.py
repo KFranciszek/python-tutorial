5439a2384a4c50d37aae46169684f581a5541b71
Pes2009 <mmadej@sigma.ug.edu.pl>
2016-03-06 23:42:58 +0100

    dalszy ciag metod.

---
+++ metody2.py
@@ -0,0 +1,20 @@
+say = ['hey','now','brown','cow']
+
+print say
+
+print say.index('brown') # pokazuje nam ktory indeks ma element "brown" w liscie say
+
+say.insert(2,'hoss') # wpycha na druga pozycje slowo "hoss"
+
+print say
+
+say.pop(1) # usuwa z pozycji pierwszej element ('now') w tym przypadku 
+print say
+
+say.remove('brown') # usuwa slowo 'brown' z elementu listy
+
+print say
+
+say.reverse() # uklada elementy alfabetycznie!!
+
+print say
