1662f7fa1f05965be0b92dff338893de218b0b11
Pes2009 <mmadej@sigma.ug.edu.pl>
2016-03-06 22:55:29 +0100

    lekcja 12 dalej cwiczymy listy.

---
+++ more-list-functions.py
@@ -0,0 +1,18 @@
+print len("ziomeczek") # zwraca dlugosc wyrazu ( znakow )
+
+numbers = [8,1,4,17,28,165,7]
+print len(numbers) # zwraca liczbe elementow listy 
+
+print max(numbers) # najwieksza liczba z listy
+
+print min(numbers) # najmniejsza liczba z funkcji 
+
+
+print list("Martin") # zwraca kazdy znak osobno jako lista
+
+numbers[3]=77 # zmieniamy 4 element 
+
+print numbers
+
+del numbers[3] # usuwamy 4 element z listy
+print numbers # lista juz go nie zawiera i sie skrocila o 1 pozycje
