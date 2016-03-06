e620870edf1bbb7b88899f5c5c2f8fdde68c7044
Pes2009 <mmadej@sigma.ug.edu.pl>
2016-03-06 23:24:28 +0100

    Poznajemy metody podstawy.

---
+++ metoty.py
@@ -0,0 +1,24 @@
+# yourface.punch()  gdzie ma byc objekt, co chcemy zrobic ( i argumenty)
+
+face = [21,18,30]
+print face
+
+face.append(45) # dodajemy element 45 na koncu listy
+
+print face
+
+apples = ['i','love','apples','apples','now']
+
+print apples.count("apples") # zlicza ile razy dany wyraz wystepuje w liscie
+
+print apples.count("bow") # tu tak samo zlicza i zwraca zero bo "bow" nie znajduje sie w zadnym elemencie
+
+two = [4,5,6]
+
+print two
+ 
+one = [1,2,3]
+
+one.extend(two)
+
+print one
