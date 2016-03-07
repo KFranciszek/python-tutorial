14c92a183623769808e35fe912c24f1fb427c938
Pes2009 <mmadej@sigma.ug.edu.pl>
2016-03-07 13:55:15 +0100

    warunki else,elif,if...

---
+++ else-and-elif.py
@@ -0,0 +1,10 @@
+fish="tuna"
+
+if fish == "bass":
+	print 'this is a fish alright'
+elif fish == "slamon":
+	print 'I hope i dont get SLAMonilla poissnin'
+elif fish == "tuna":
+	print "easy there tuna boat"
+else:
+	print 'I dont know what this is'
---
+++ if-statement.py
@@ -0,0 +1,6 @@
+tuna = "fish"
+
+if tuna == "bass":
+	print 'this is a fish alright'
+else:
+		print 'so badly...'
