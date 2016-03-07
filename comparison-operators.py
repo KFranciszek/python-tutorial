cc34047177dfdcc1a52ad13a691942954d672e5d
Pes2009 <mmadej@sigma.ug.edu.pl>
2016-03-07 14:33:22 +0100

    poznajemy rozne operatory oraz zagniezdzanie warunku if.

---
+++ comparison-operators.py
@@ -0,0 +1,22 @@
+print 9 < 7 # poznajemy operatory nudy...
+
+print 9 <= 9
+
+print 9 != 7
+
+one = [21,22,23]
+
+two = [21,22,23]
+
+print one == two # zwroci prawde bo maja taka sama liste
+
+print one is two # zwroci falsz bo to sa jednak inne tablice
+
+three = four = [3,4,5] # tu zwroci prawde bo przypisujemy jednoczesnie te same dane lista
+
+
+print three is four
+
+
+pizza = "pizzahut"
+print 's' in pizza # zwroci falsz bo 's' nie znajduje sie w  'pizzahut'
---
+++ nesting-statements.py
@@ -0,0 +1,11 @@
+think = "animal"
+animal = "cat"
+
+if think == "animal":
+	if animal == "cat": # zagniezdzamy warunek if
+		print 'this is a cat'
+	else:
+			print 'i dont know what this animal is'
+
+else:
+	print 'i dont know what this thing is'
